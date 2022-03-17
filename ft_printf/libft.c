3df5842e2a1982870ec5047b2d879bc1969fae56
nosamanz <62108864+nosamanz@users.noreply.github.com>
2022-03-17 12:07:34 +0300

    Add files via upload

---
+++ ft_printf/libft.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oozcan <oozcan@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/09 14:58:54 by oozcan            #+#    #+#             */
+/*   Updated: 2022/03/09 15:47:25 by oozcan           ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+static char	*set_str(int n, int *bas)
+{
+	char	*str;
+
+	*bas = 1;
+	if (n < 0)
+	{
+		n *= -1;
+		(*bas)++;
+	}
+	while (n >= 10)
+	{
+		n /= 10;
+		(*bas)++;
+	}
+	str = (char *)malloc(sizeof(char) * (*bas + 1));
+	if (!str)
+		return (NULL);
+	str[0] = '-';
+	str[(*bas)] = '\0';
+	return (str);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*str;
+	int		bas;
+
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
+	str = set_str(n, &bas);
+	if (n < 0)
+		n *= -1;
+	if (!str)
+		return (NULL);
+	while (n >= 10)
+	{
+		str[--bas] = n % 10 + '0';
+		n = n / 10;
+	}
+	str[--bas] = n % 10 + '0';
+	return (str);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*s2;
+	int		len;
+	int		i;
+
+	len = ft_strlen(s1);
+	s2 = malloc(sizeof(char) * len + 1);
+	if (s2 == NULL)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		s2[i] = s1[i];
+		i++;
+	}
+	s2[i] = '\0';
+	return (s2);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	x;
+
+	x = 0;
+	while (s[x])
+		x++;
+	return (x);
+}
